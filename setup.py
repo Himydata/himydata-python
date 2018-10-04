3c03f9237ca27da6795620c74b33d6e213456a41
David Bessoudo <david@himydata.com>
2018-10-04 11:04:06 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+long_description = (open('README').read() +
+    '\n\n' + open('HISTORY.txt').read())
+
+setup(
+        name='dataiku-api-client',
+        version=VERSION,
+        license="Apache Software License",
+        packages=["himydata", "himydata.utils"],
+        description="Python library for Himydata Platform",
+        long_description=long_description,
+        author="Himydata",
+        author_email="support@himydata.com",
+        url="https://www.himydata.com",
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Topic :: Software Development :: Libraries',
+            'Programming Language :: Python',
+            'Operating System :: OS Independent'
+        ],
+        install_requires = [
+            "requests>=2",
+            "python-dateutil"
+            ""
+        ]
+     )
